479992e66fee2baeab5b7c239527987e0e154de3
Pedro Henrique Almeida Castro <pcastrodev@gmail.com>
2025-11-20 14:50:15 -0300

    fix: recriar arquivo database.js da configuração do MongoDB

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+connectDatabase();
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
