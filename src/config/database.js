82f211759082fba046453d903f4fcf3fa6369d66
Pedro Henrique Almeida Castro <pcastrodev@gmail.com>
2025-11-20 14:44:30 -0300

    fix: recriar arquivo database.js da configuração do MongoDB

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Conectado ao MongoDB Atlas');
+  } catch (error) {
+    console.log('Erro ao conectar no MongoDB:', error);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDatabase;
