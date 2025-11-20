3b4a097789ea8f2be5ffbbca9a33f912f7eb7106
Pedro Henrique Almeida Castro <pcastrodev@gmail.com>
2025-11-20 15:46:47 -0300

    feat: middleware requireAuth para proteger rotas com JWT

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 const { register, login } = require('../controllers/authController');
+const requireAuth = require('../middlewares/requireAuth');
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/me', requireAuth, (req, res) => {
+  return res.status(200).json({ user: req.user });
+});
 
 module.exports = router;
