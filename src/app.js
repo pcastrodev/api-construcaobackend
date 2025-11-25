a6a06f257b6b51561611fd51e52d9c69e62738fe
Gabriel Gomes Almeida <ggoalmeidaa@gmail.com>
2025-11-24 22:07:14 -0300

    feat: CRUD de produtos implementado (Issue Gabriel)

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const swaggerDocument = require('./docs/swagger.json');
 const app = express();
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
